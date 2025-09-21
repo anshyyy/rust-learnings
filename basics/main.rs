0db5f26bd7e0261f0109ffd159c40fe224b6c2cf
anshyyy <anshuman9998@gmail.com>
2025-09-21 14:46:49 +0530

    Add comments to main.rs for compilation instructions and macro explanation

--- basics/main.rs
+++ basics/main.rs
@@ -1,3 +1,15 @@
+
+
+// compile the code if youre not using cargo 
+// rustc main.rs
+//then run the code with ./main
+
+
+
+// println! is a macro that prints the string to the console
+// its not a function, its a macro, well learn about macros later
+
+
 fn main() {
    println!("Hello, world!");
 }
